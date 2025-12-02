0fb092a8a0dea93eceaf7cc76ca612b505e2a8ea
AmaoLearning <darksoul1remaked@outlook.com>
2025-12-02 23:35:06 +0800

    Add specific fast3dgs scenes for further validation

---
+++ train_original.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0 OAR_JOB_ID=truck python train.py -s /root/datasets/tandt_db/tandt/truck -m output/truck  --eval --densification_interval 500  --optimizer_type default --test_iterations 30000  --highfeature_lr 0.04 --grad_abs_thresh 0.0009 --mult 0.7 
+CUDA_VISIBLE_DEVICES=0 OAR_JOB_ID=train python train.py -s /root/datasets/tandt_db/tandt/train -m output/train --eval --densification_interval 500  --optimizer_type default --test_iterations 30000  --highfeature_lr 0.042 --grad_abs_thresh 0.0015 --dense 0.01 --mult 0.7 
+CUDA_VISIBLE_DEVICES=0 OAR_JOB_ID=playroom python train.py -s /root/datasets/tandt_db/db/playroom -m output/playroom --eval --densification_interval 500  --optimizer_type default --test_iterations 30000  --highfeature_lr 0.0015 --dense 0.003 --mult 0.7
+CUDA_VISIBLE_DEVICES=0 OAR_JOB_ID=drjohnson python train.py -s /root/datasets/tandt_db/db/drjohnson -m output/drjohnson --eval --densification_interval 500  --optimizer_type default --test_iterations 30000  --highfeature_lr 0.0025 --grad_abs_thresh 0.0012 --dense 0.013 --mult 0.7 
+
+CUDA_VISIBLE_DEVICES=0 python render.py -m output/truck --skip_train --mult 0.7
+CUDA_VISIBLE_DEVICES=0 python render.py -m output/train --skip_train --mult 0.7
+CUDA_VISIBLE_DEVICES=0 python render.py -m output/playroom --skip_train --mult 0.7
+CUDA_VISIBLE_DEVICES=0 python render.py -m output/drjohnson --skip_train --mult 0.7
+
+CUDA_VISIBLE_DEVICES=0 python metrics.py -m output/truck
+CUDA_VISIBLE_DEVICES=0 python metrics.py -m output/train
+CUDA_VISIBLE_DEVICES=0 python metrics.py -m output/playroom
+CUDA_VISIBLE_DEVICES=0 python metrics.py -m output/drjohnson
