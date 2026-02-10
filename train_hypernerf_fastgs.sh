802d753a68c20a6c3d5acb5d13da3da7df5deff0
AmaoLearning <darksoul1remaked@outlook.com>
2026-02-10 16:17:43 +0000

    Debug hypernerf scripts

--- train_hypernerf_fastgs.sh
+++ train_hypernerf_fastgs.sh
@@ -4,7 +4,7 @@ python train.py -s /root/datasets/hypernerf/interp_americano -m output/interp_am
 python train.py -s /root/datasets/hypernerf/interp_split-cookie -m output/interp_split-cookie --eval --iterations 30000
 python train.py -s /root/datasets/hypernerf/vrig-chicken -m output/vrig-chicken --eval --iterations 30000
 python train.py -s /root/datasets/hypernerf/interp_torchocolate -m output/interp_torchocolate --eval --iterations 30000
-python train.py -s /root/datasets/hypernerf/interp_cut-lemon1 -m output/interp_cut-lemon --eval --iterations 30000
+python train.py -s /root/datasets/hypernerf/interp_cut-lemon1 -m output/interp_cut-lemon1 --eval --iterations 30000
 python train.py -s /root/datasets/hypernerf/interp_hand1-dense-v2 -m output/interp_hand1-dense-v2 --eval --iterations 30000
 python train.py -s /root/datasets/hypernerf/vrig-3dprinter -m output/vrig-3dprinter --eval --iterations 30000
 
