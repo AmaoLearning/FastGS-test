187a31139293668a8169b2e7e61d198087357ecf
AmaoLearning <darksoul1remaked@outlook.com>
2026-03-07 17:29:05 +0800

    DEBUG: update training scripts

--- train_4dgs.sh
+++ train_4dgs.sh
@@ -1,10 +1,10 @@
 # Neur3D Dataset
-python train.py -s /root/datasets/Neu3D/coffee_martini -m output_4dgs/coffee_martini --eval --iterations 30000 --lazy_load --deform_type 4dgs
-python train.py -s /root/datasets/Neu3D/cook_spinach -m output_4dgs/cook_spinach --eval --iterations 30000 --lazy_load --deform_type 4dgs
-python train.py -s /root/datasets/Neu3D/cut_roasted_beef -m output_4dgs/cut_roasted_beef --eval --iterations 30000 --lazy_load --deform_type 4dgs
-python train.py -s /root/datasets/Neu3D/flame_steak -m output_4dgs/flame_steak --eval --iterations 30000 --lazy_load --deform_type 4dgs
-python train.py -s /root/datasets/Neu3D/sear_steak -m output_4dgs/sear_steak --eval --iterations 30000 --lazy_load --deform_type 4dgs
-python train.py -s /root/datasets/Neu3D/flame_salmon_1 -m output_4dgs/flame_salmon_1 --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/coffee_martini -m output_4dgs/coffee_martini --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/cook_spinach -m output_4dgs/cook_spinach --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/cut_roasted_beef -m output_4dgs/cut_roasted_beef --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/flame_steak -m output_4dgs/flame_steak --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/sear_steak -m output_4dgs/sear_steak --eval --iterations 30000 --lazy_load --deform_type 4dgs
+python train.py -s /remote-home/panyicheng/datasets/Neu3D/flame_salmon_1 -m output_4dgs/flame_salmon_1 --eval --iterations 30000 --lazy_load --deform_type 4dgs
 
 python render.py -m output_4dgs/coffee_martini --mode render --skip_train
 python render.py -m output_4dgs/cook_spinach --mode render --skip_train
